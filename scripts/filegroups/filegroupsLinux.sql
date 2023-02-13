47cc87e5a0a8b75dbce7ac77fc02974478148e56
theobimel <theobimel@gmail.com>
2023-02-13 02:27:40 +0000

    fix: rename filegroups + errors

--- scripts/filegroups/filegroupsLinux.sql
+++ scripts/filegroups/filegroupsLinux.sql
@@ -3,8 +3,8 @@ GO
 
 create database WWIGlobal
 on primary (
-    name = 'wwi_primary' -- system objects
-    filename = '/var/opt/mssql/data/wwi_primary.mdf',
+    name = 'wwiglobal_primary', -- system objects
+    filename = '/var/opt/mssql/data/wwiglobal_primary.mdf',
     size = 10MB,
     maxsize = 20MB,
     filegrowth = 10MB
@@ -13,8 +13,8 @@ on primary (
 -- Features: +/-Access, +/-Read, -Write
 -- Tables: Continent, Country, StateProvince, SalesTerritory, State_Country, CityName, City, Token, Error, Logistic, TaxRate, Currency, Color, Package, BusinessCategory
 filegroup WWIGlobal_fg1 (
-    name = 'wwi_fg1',
-    filename = '/var/opt/mssql/data/wwi_fg1.ndf',
+    name = 'wwiglobal_fg1',
+    filename = '/var/opt/mssql/data/wwiglobal_fg1.ndf',
     size = 10MB,
     maxsize = 30MB,
     filegrowth = 10MB
@@ -23,8 +23,8 @@ filegroup WWIGlobal_fg1 (
 -- Features: +Write, +Access
 -- Tables: SalesOrderHeader, Employee, ErrorLogs, ColumnInfo, Estimation, SystemUser, Discount, ProductModel, Size, Contact, BuyingGroup, Transport
 filegroup WWIGlobal_fg2 (
-    name = 'wwi_fg2',
-    filename = '/var/opt/mssql/data/wwi_fg1.ndf'
+    name = 'wwiglobal_fg2',
+    filename = '/var/opt/mssql/data/wwiglobal_fg1.ndf',
     size = 10MB,
     maxsize = 50MB,
     filegrowth = 20MB
@@ -33,15 +33,15 @@ filegroup WWIGlobal_fg2 (
 -- Features: +Read, +Write
 -- Tables: SalesOrderDetails, CurrencyRate, Salesman, PostalCode, Address, Customer, Color_Product, Product, Brand
 filegroup WWIGlobal_fg3 (
-    name = 'wwi_fg3_1',
-    filename = '/var/opt/mssql/data/wwi_fg1.ndf'
+    name = 'wwiglobal_fg3_1',
+    filename = '/var/opt/mssql/data/wwiglobal_fg1.ndf',
     size = 20MB,
-    maxsize 60MB,
+    maxsize = 60MB,
     filegrowth = 20MB
 ),
 (
-    name = 'wwi_fg3_2',
-    filename = '/var/opt/mssql/data/wwi_fg1.ndf'
+    name = 'wwiglobal_fg3_2',
+    filename = '/var/opt/mssql/data/wwiglobal_fg1.ndf',
     size = 30MB,
     maxsize = 80MB,
     filegrowth = 25MB
@@ -49,8 +49,8 @@ filegroup WWIGlobal_fg3 (
 
 -- Log File
 log on (
-    name = 'wwi_log',
-    filename = '/var/opt/mssql/data/wwi_log.ldf',
+    name = 'wwiglobal_log',
+    filename = '/var/opt/mssql/data/wwiglobal_log.ldf',
     SIZE = 10MB,
     MAXSIZE = 50MB,
     FILEGROWTH = 20MB
